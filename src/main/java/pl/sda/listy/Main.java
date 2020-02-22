e9df51574f1d6ff6bf7c27ffbc6b9c9c9171a1a0
Angelika A <angelikabadura@tlen.pl>
2020-02-22 12:59:18 +0100

    implementacja listy

---
+++ src/main/java/pl/sda/listy/Main.java
@@ -0,0 +1,32 @@
+package pl.sda.listy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lista lista = new ListaImplementacja();
+
+        // tak zapisujemy korzystając z interfejsu Lista - klasa Lista Implementacje implementuje z interfejsu Lista
+        // ListaImplementacja lista = new ListaImplementacja();
+
+        lista.dodajElement(212);
+
+    }
+}
+
+
+
+
+
+
+
+
+        //int tab[] = new int[100];
+
+        //List<Integer> list = new ArrayList<Integer>();
+        //list.add(21);
+        //list.add(222);
+        //list.add(12312);
+        //list.add(332);
